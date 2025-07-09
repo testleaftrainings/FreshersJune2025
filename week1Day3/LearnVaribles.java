08b525cb108b03838b166e0393d64453a9acbb9c
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2025-07-09 18:45:05 +0530

    Add files via upload

---
+++ week1Day3/LearnVaribles.java
@@ -0,0 +1,47 @@
+package week1.day3.learnVariables;
+
+public class LearnVaribles {
+
+	static String title="java";
+	
+	int age =25;
+	
+	
+	
+	public void method() {
+		System.out.println(age);
+		String name="vinoth";
+	}
+	
+	public void method1() {
+		//System.out.println(name);
+	}
+	
+
+	
+	
+	
+	public static void main(String[] args) {
+		
+		int number =123;
+		
+		//variable are act like a container to store the data
+		
+		
+		// static variable, Global variable, Local variable
+		//eg : int a=10   int -->DataType  a->variableName  10-->value/data/Info/input
+//		byte b=128;
+//		short sh=;
+//		
+//		System.out.println(sh);
+		System.out.println(number);
+		LearnVaribles obj = new LearnVaribles();
+		System.out.println(LearnVaribles.title);
+		//System.out.println(LearnVaribles.age);
+		
+		
+		
+		
+		
+	}
+}
